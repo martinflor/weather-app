a39e6cd950a88c0f5ac244c0463188d1e7682f5f
florian.martin <florian.martin@johncockerill.com>
2024-10-23 09:46:19 +0200

    Weather App basic style

---
+++ query.js
@@ -0,0 +1,7 @@
+const search = document.querySelector('.search button');
+
+
+search.addEventListener('click', () => {
+    const api_key = ''
+    const city = document.querySelector('.search input')
+});
